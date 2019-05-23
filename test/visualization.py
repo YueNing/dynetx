aaff50e94c80a8f14ff7122da1d9373df9b72bfd
naodongbanana <n1085633848@outlook.com>
2019-05-23 13:50:17 +0200

    move file to test directory

--- test/visualization.py
+++ test/visualization.py
@@ -2,6 +2,7 @@ import glovar
 import multiprocessing
 import time
 from test_drawing import *
+import psutil
 
 class RecallClockProcess(multiprocessing.Process):
     def __init__(self, interval, world_recall_reuslt):
@@ -10,6 +11,16 @@ class RecallClockProcess(multiprocessing.Process):
         self._world_recall = self._world_recall_fun()
         self.world_recall_reuslt = world_recall_reuslt
 
+    def stop(self):
+            print('Process suspend  id %s ' %(self.pid))
+            p = psutil.Process(self.pid)
+            p.suspend()
+
+    def wake(self):
+            print ('Process resume  id  %s ' %(self.pid))
+            p = psutil.Process(self.pid)
+            p.resume()
+
     def _world_recall_fun(self):
         world = 0
         while True:
@@ -17,17 +28,16 @@ class RecallClockProcess(multiprocessing.Process):
             world +=1
             
     def run(self):
+        print ('当前运行进程PID :  %s  '  %self.pid) 
         while True:
             glovar.world_recall = next(self._world_recall)
             print('world_recall',glovar.world_recall)
             print("the time is {0}:world_recall_fun {1}".format(time.ctime(),glovar.world_recall))
             if self.world_recall_reuslt:
                 self.world_recall_reuslt.value = glovar.world_recall
+            # self.stop()
             time.sleep(self.interval)
-
-def func(i, d):
-    d['glovar.world_recall'] = i + 100
-    print(d.values())
+            # self.wake()
 
 if __name__ == '__main__':
     world_recall_reuslt = glovar.world_recall_reuslt
