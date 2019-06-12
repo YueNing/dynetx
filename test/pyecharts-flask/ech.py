0ceaca88a4deab8a16ce576ef47c244833c1749a
naodongbanana <n1085633848@outlook.com>
2019-06-12 18:42:59 +0200

    new layout

--- test/pyecharts-flask/ech.py
+++ test/pyecharts-flask/ech.py
@@ -3,11 +3,12 @@ import os
 from dataclasses import dataclass
 
 from pyecharts import options as opts
-from pyecharts.charts import Graph, Page, Liquid
+from pyecharts.charts import Graph, Page, Liquid, Bar3D, Grid
 from pyecharts.charts import Geo
 from pyecharts.commons.types import Numeric, Optional, Sequence, Union
 from pyecharts.globals import ChartType, SymbolType
-
+from pyecharts.components import Table
+from test import grid_horizontal
 from flask.json import jsonify
 from flask import Flask, render_template
 from app import FlaskAppWrapper
@@ -55,7 +56,10 @@ class DrawPyechart(object):
         self.a = FlaskAppWrapper(name, static_folder=static_folder)
         self.a.add_endpoint(rule='/', endpoint='home', view_func=self.index)
         self.a.add_endpoint(rule='/GraphChart', endpoint='GraphChart', view_func=self._graph_with_opts)
+        self.a.add_endpoint(rule='/Liquid', endpoint='Liquid', view_func=self._get_liquid_chart)
         self.a.add_endpoint(rule='/GraphDynamicData', endpoint='GraphDynamicData', view_func=self._graph_with_opts_dyn)
+        self.a.add_endpoint(rule='/Bar3dData', endpoint='Bar3dData', view_func=self._bar3d_with_opts)
+        self.a.add_endpoint(rule='/Grid', endpoint='Grid', view_func=self._test_grid)
         self.a.run()
     
     def _init(self):
@@ -77,12 +81,11 @@ class DrawPyechart(object):
 
 
     def index(self):
-        liquid = self._get_liquid_chart()
-        return render_template("index.html", myechart=liquid.render_embed())
-    
+        return render_template("index.html")
+
     @staticmethod
     def _get_liquid_chart():
-        return liquid_base()
+        return liquid_base().dump_options()
     
     def _get_layer_sizes(self):
         return [len(node) for node in self.node_name]
@@ -98,19 +101,47 @@ class DrawPyechart(object):
             Graph()
             .add("", nodes, links, self.categories, edge_symbol=['', 'arrow'], edge_symbol_size=10,layout='none', repulsion=4000,
             emphasis_itemstyle_opts=self.config.linestyleopts[0])
-            .set_global_opts(title_opts=opts.TitleOpts(title="Graph-Negmas-Contract-Signed"))
+            .set_global_opts(title_opts=opts.TitleOpts(title=""))
         )
         return c.dump_options()
     
+    @staticmethod
+    def _bar3d_with_opts():
+        return bar3d_base().dump_options()
+        # return jsonify({'type':'line3d'})
+
+    def init_bar3d_base(self):
+        from example.commons import Faker
+        from pyecharts import options as opts
+        from pyecharts.charts import Bar3D
+        c = (
+            Bar3D()
+            .add(
+                "",
+                [],
+                xaxis3d_opts=opts.Axis3DOpts(Faker.clock, type_="category"),
+                yaxis3d_opts=opts.Axis3DOpts(Faker.week_en, type_="category"),
+                zaxis3d_opts=opts.Axis3DOpts(type_="value"),
+            )
+            .set_global_opts(
+                visualmap_opts=opts.VisualMapOpts(max_=20),
+                title_opts=opts.TitleOpts(title=""),
+            )
+        )
+        return c
+
     def _graph_with_opts_dyn(self):
         g = negmas_draw.negmas_add_edges(self.config.graph, self.config.layer_sizes, node_name=self.config.node_name)
         edges = [(edge[0], edge[1]) for edge in g.edges]
         links = [opts.GraphLink(source=edge[0], target=edge[1]) for edge in edges]
-        print(links[0].opts)
         result = []
         for link in links:
             result.append({"source": link.opts['source'], "target": link.opts['target']})
         return jsonify(result)
+
+    @staticmethod
+    def _test_grid():
+        return test_grid().dump_options()
 
 def get_layer_sizes(nodes):
     return [len(node) for node in nodes]
@@ -119,6 +150,28 @@ def index():
     # liquid = graph_with_opts()
     # return render_template("index.html", myechart=liquid.render_embed())
     return 'hello'
+
+def bar3d_base() -> Bar3D:
+        import random
+        from example.commons import Faker
+        from pyecharts import options as opts
+        from pyecharts.charts import Bar3D
+        data = [(i, j, random.randint(0, 12)) for i in range(6) for j in range(24)]
+        c = (
+            Bar3D()
+            .add(
+                "",
+                [[d[1], d[0], d[2]] for d in data],
+                xaxis3d_opts=opts.Axis3DOpts(Faker.clock, type_="category"),
+                yaxis3d_opts=opts.Axis3DOpts(Faker.week_en, type_="category"),
+                zaxis3d_opts=opts.Axis3DOpts(type_="value"),
+            )
+            .set_global_opts(
+                visualmap_opts=opts.VisualMapOpts(max_=20),
+                title_opts=opts.TitleOpts(title=""),
+            )
+        )
+        return c
 
 def draw_with_pyecharts():
     """
@@ -135,7 +188,7 @@ def geo_base() -> Geo:
         .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
         .set_global_opts(
             visualmap_opts=opts.VisualMapOpts(),
-            title_opts=opts.TitleOpts(title="Geo-基本示例"),
+            title_opts=opts.TitleOpts(title=""),
         )
     )
     return c
@@ -148,7 +201,7 @@ def geo_visualmap_piecewise() -> Geo:
         .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
         .set_global_opts(
             visualmap_opts=opts.VisualMapOpts(is_piecewise=True),
-            title_opts=opts.TitleOpts(title="Geo-VisualMap（分段型）"),
+            title_opts=opts.TitleOpts(title=""),
         )
     )
     return c
@@ -196,7 +249,7 @@ def graph_with_opts() -> Graph:
         Graph()
         .add("", nodes, links, categories, edge_symbol=['', 'arrow'], edge_symbol_size=10,layout='none', repulsion=4000,
         emphasis_itemstyle_opts=linestyleopts[0])
-        .set_global_opts(title_opts=opts.TitleOpts(title="Graph-GraphNode-GraphLink"))
+        .set_global_opts(title_opts=opts.TitleOpts(title=""))
     )
     return c
 
@@ -204,12 +257,38 @@ def liquid_base() -> Liquid:
     c = (
         Liquid()
         .add("lq", [0.6, 0.7])
-        .set_global_opts(title_opts=opts.TitleOpts(title="Process"))
+        .set_global_opts(title_opts=opts.TitleOpts(title=""))
     )
     return c
 
-# graph_with_opts().render('k.html')
+
+def table_base() -> Table:
+    from pyecharts.options import ComponentTitleOpts
+
+    table = Table()
+
+    headers = ["Agent name", "Balance", "Signed Contracts", "Executed Contracts"]
+    rows = [
+        ["Brisbane", 5905, 1857594, 1146.4],
+        ["Adelaide", 1295, 1158259, 600.5],
+        ["Darwin", 112, 120900, 1714.7],
+        ["Hobart", 1357, 205556, 619.5],
+        ["Sydney", 2058, 4336374, 1214.8],
+        ["Melbourne", 1566, 3806092, 646.9],
+        ["Perth", 5386, 1554769, 869.4],
+    ]
+    table.add(headers, rows).set_global_opts(
+        title_opts=ComponentTitleOpts(title="", 
+                title_style={"style": "font-size: 18px; font-weight:bold;"})
+    )
+    return table
+
+
+def test_grid() -> Grid:
+    return grid_horizontal()
+
 if __name__ == '__main__':
     g = DiGraph()
     nodes = [['m_1', 'm_2', 'm_3'], ['f_1', 'f_2'], ['g_1', 'g_2'], ['c_1', 'c_2']]
     DrawPyechart(g=g, node_name=nodes, name='DrawPyecharts')
+    # table_base().render()
